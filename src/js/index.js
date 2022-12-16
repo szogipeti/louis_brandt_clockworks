28c84f9c9f044e80c096aee26cfa61f266143ae3
szogipeti <szogipeti02@gmail.com>
2022-12-16 10:44:04 +0100

    Fix responsitvity issues

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,7 @@ async function onLoad(){
     await FetchHelper.read('items').then(data => items = data);
     await FetchHelper.read('brands').then(data => brands = data);
     await FetchHelper.read('tags').then(data => tags = data);
-    await FetchHelper.read('images').then(data => tags = data);
+    await FetchHelper.read('images').then(data => imgs = data);
     loadNavbar(tags, brands);
     loadCards(items);
 }
