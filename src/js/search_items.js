effd296feab8e01f1247486cb4360039e8ecaa3f
szogipeti <szogipeti02@gmail.com>
2023-01-04 11:11:14 +0100

    Add filtering based on tags

--- src/js/search_items.js
+++ src/js/search_items.js
@@ -22,13 +22,12 @@ async function onLoad(){
     const tag = urlParams.get('tag');
     const brand = urlParams.get('brand');
     let fetchParams = 'items?';
-    console.table(tags);
-    if(tag != null){
-        fetchParams += `${tag}`;
-    }
     if(brand != null){
         fetchParams += `brandId=${brands.filter(x=>x.name==brand)[0]['id']}`;
     }
     await FetchHelper.read(fetchParams).then(data => items = data);
+    if(tag != null){
+        items = items.filter(x=>x.tagIds.includes(tags.filter(x=>x.name==tag)[0]['id']));
+    }
     loadCards(items);
 }
