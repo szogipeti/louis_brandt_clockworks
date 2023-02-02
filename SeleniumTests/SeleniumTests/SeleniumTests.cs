71cd85a045bcabde1fbfe0e730122fefa6dc8cf8
szogipeti <szogipeti02@gmail.com>
2023-02-02 09:54:28 +0100

    Test navbar

--- SeleniumTests/SeleniumTests/SeleniumTests.cs
+++ SeleniumTests/SeleniumTests/SeleniumTests.cs
@@ -1,5 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SeleniumTests
 {
@@ -15,8 +18,21 @@ namespace SeleniumTests
             driver.Navigate().GoToUrl("http://localhost:8080");
         }
         [TestMethod]
-        public void TestMethod1()
+        public void GoToShoppingPage()
         {
+            driver.FindElement(By.CssSelector("nav li a")).Click();
+            Assert.AreEqual("Items in your cart:", driver.FindElement(By.CssSelector("#item-container h2")).Text);
+        }
+        [TestMethod]
+        public void GoToSearchPageEmpty()
+        {
+            driver.FindElement(By.CssSelector("nav li input[type=submit]")).Click();
+            System.Threading.Thread.Sleep(500);
+            ICollection<IWebElement> webElements = driver.FindElements(By.CssSelector("#item-container h5"));
+            Assert.AreEqual("ora1", webElements.ElementAt(0).Text);
+            Assert.AreEqual("ora2", webElements.ElementAt(1).Text);
+            Assert.AreEqual("ora4", webElements.ElementAt(2).Text);
+            Assert.AreEqual("ora3", webElements.ElementAt(3).Text);
         }
         [TestCleanup]
         public void ChromeDriverCleanup()
